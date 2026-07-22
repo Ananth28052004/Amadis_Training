603b8e744c33ea127325706fad6389eacbe521fc
Ananth <asananth04@gmail.com>
2026-07-23 00:27:21 +0530

    Day-6

---
+++ Day-6/Task-4/ClassList.js
@@ -0,0 +1,11 @@
+let h1=document.getElementById("head");
+h1.classList.add("h1class");
+
+let h1remove=document.getElementById("secondtag");
+h1remove.classList.remove("h1class");
+
+let font=document.getElementById("p");
+font.classList.toggle("sec")
+
+let h3id=document.getElementById("h3id");
+console.log(h3id.classList.contains("h3s"))
